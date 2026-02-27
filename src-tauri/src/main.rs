352c29a4bd5a75d22085bb2c37d0aae91a36410c
makka999 <mateuszk-98@wp.pl>
2026-02-27 16:39:25 +0100

    fix: comment plugin(tauri_plugin_single_instance

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -61,13 +61,6 @@ fn main() {
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
-       .plugin(tauri_plugin_single_instance::init(|app, argv, cwd| {
-            println!("{}, {argv:?}, {cwd}", app.package_info().name);
-            let _ = app
-                .get_webview_window("main")
-                .expect("no main window")
-                .set_focus();
-        })) 
         .invoke_handler(tauri::generate_handler![
             show_main_window_if_hidden,
             close_splashscreen_if_exists,
