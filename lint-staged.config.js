0900b1ffb449ccebc47fa887e70f72c3e9649c91
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2026-05-06 19:18:26 +0200

    chore(lint): migrate from eslint to oxlint

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,11 +1,9 @@
 module.exports = {
-    // This will lint and format TypeScript and                                             //JavaScript files
     "**/*.(ts|tsx|js|jsx)": filenames => [
-        `bunx eslint --fix ${filenames.join(" ")}`,
+        `bunx oxlint --fix ${filenames.join(" ")}`,
         `bunx prettier --write ${filenames.join(" ")}`,
     ],
 
-    // this will Format MarkDown and JSON
     "**/*.(md|json)": filenames =>
         `bunx prettier --write ${filenames.join(" ")}`,
 };
