9d7b1729d68807478256ef2ef448bf412530a484
Marcel Gańczarczyk <ganczarczyk.marcel@gmail.com>
2026-05-06 19:52:43 +0200

    fix(tsconfig): include vite.config.ts so esModuleInterop applies

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-const host = process.env.TAURI_DEV_HOST;
+const host = process.env["TAURI_DEV_HOST"];
 
 export default defineConfig({
     plugins: [react()],
